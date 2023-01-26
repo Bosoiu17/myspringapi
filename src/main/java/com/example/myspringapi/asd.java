121ce4ea8f7dbd962c2d2ccc8aabfd2be66f56e5
George <bosoiu17@gmail.com>
2023-01-26 16:11:05 +0100

    invio prova2

--- src/main/java/com/example/myspringapi/asd.java
+++ src/main/java/com/example/myspringapi/asd.java
@@ -3,4 +3,6 @@ package com.example.myspringapi;
 public class asd {
 
     String prova;
+
+    String prova1;
 }
--- src/main/java/com/example/myspringapi/prova.java
+++ src/main/java/com/example/myspringapi/prova.java
@@ -6,5 +6,7 @@ public class prova {
 
     String prova2;
 
+    String prova3;
+
 
 }
